f50f980b10b713d2afd56da742a89cac49ff1119
hxonxthai <hxonxthai@gmail.com>
2025-03-28 23:53:36 +0900

    감자 과제1 연습파일
    제발,,,,

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import styled from "styled-components";
+
+function App() {
+  const onCLickEvent = () => {
+    console.log("Clicked!!");
+  };
+
+  return (
+    <Layout>
+      <div
+        onClick={() => {
+          onCLickEvent();
+        }}
+        style={{width:'281px', height:'88px'}}
+      >
+        <img src="https://static.univstore.com/web/image/logo.png" />
+      </div>
+      <div>
+      검색
+      </div>
+      <div>
+      로그인
+      </div>
+    </Layout>
+  );
+}
+
+export default App;
+
+const Layout = styled.div`
+  display: flex;
+  width: 100%;
+  justify-content: space-between;
+`;
+
+const StyledInput = styled.input``;
